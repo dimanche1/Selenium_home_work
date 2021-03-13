2428d12c12fd31c5d3489acf9ce1fe08014727b9
Dmitry <dimanche5014@gmail.com>
2021-03-13 19:46:36 +0300

    hw_9 second exercise added

--- hw_1/hw_9/src/test/java/Exercise9.java
+++ hw_1/hw_9/src/test/java/Exercise9.java
@@ -1,4 +1,3 @@
-import com.google.common.collect.Ordering;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,7 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -26,13 +24,17 @@ public class Exercise9 {
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
-    @Test
-    public void testExercise9() {
-
+    private void login() {
         driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
+    }
+
+    @Test
+    public void testExercise9_1() {
+
+        login();
 
         driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
         List<WebElement> countries = driver.findElements(By.cssSelector("form[name=countries_form] tr.row"));
@@ -55,6 +57,28 @@ public class Exercise9 {
                 List<String> zoneNames = new ArrayList<>();
                 driver.findElements(By.cssSelector("#table-zones tr td:nth-child(3) input[name*=zones]"))
                     .forEach(zone_el -> zoneNames.add(zone_el.getText()));
+                assertTrue(TestBase.isSortedNaturally(zoneNames));
+            });
+        }
+    }
+
+    @Test
+    public void testExercise9_2() {
+
+        login();
+
+        driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
+
+        List <String> geoZones = new ArrayList<>();
+        driver.findElements(By.cssSelector("form[name=geo_zones_form] tr.row td:nth-child(3) a"))
+                .forEach(el -> geoZones.add(el.getAttribute("href")));
+
+        if(geoZones.size() > 0) {
+            geoZones.forEach(zone -> {
+                driver.get(zone);
+                List<String> zoneNames = new ArrayList<>();
+                driver.findElements(By.cssSelector("#table-zones td:nth-child(3) select [selected=selected]"))
+                        .forEach(zone_el -> zoneNames.add(zone_el.getText()));
                 assertTrue(TestBase.isSortedNaturally(zoneNames));
             });
         }
